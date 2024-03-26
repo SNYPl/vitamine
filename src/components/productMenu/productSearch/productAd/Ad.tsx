61f5ae3724a42363ac9688ad8dd19d050543f067
Davit Metreveli <davitmetreveli@pulsarai.ge>
2024-03-26 17:19:03 +0400

    change offer

--- src/components/productMenu/productSearch/productAd/Ad.tsx
+++ src/components/productMenu/productSearch/productAd/Ad.tsx
@@ -6,15 +6,15 @@ const Ad = () => {
   return (
     <section className={`${styles.ad} adImage`}>
       <div className={styles.adBackground}>
-        {/* <Link href="/product?id=65cbe67306970833b5bfc330" target="_blank">
-          <Image
-            src="/images/offer.jpg"
-            width={1040}
-            height={400}
-            alt="img"
-            priority
-          />
-        </Link> */}
+        {/* <Link href="/product?id=65cbe67306970833b5bfc330" target="_blank"> */}
+        <Image
+          src="https://vitvit123.s3.eu-north-1.amazonaws.com/offer/offer.jpg"
+          width={1040}
+          height={400}
+          alt=""
+          priority
+        />
+        {/* </Link> */}
       </div>
     </section>
   );
